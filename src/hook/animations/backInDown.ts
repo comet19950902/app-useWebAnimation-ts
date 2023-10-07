bcef00cac14cc2b8435312bb7a0c4bc8e6736091
comet19950902 <goodluckydev@gmail.com>
2023-10-07 16:54:49 -0700

    remove unnecessity

--- src/hook/animations/backInDown.ts
+++ src/hook/animations/backInDown.ts
@@ -1,4 +1,4 @@
-export default {
+const backInDown = {
   keyframes: [
     { transform: "translateY(-1200px) scale(0.7)", opacity: 0.7, offset: 0 },
     { transform: "translateY(0px) scale(0.7)", opacity: 0.7, offset: 0.8 },
@@ -6,3 +6,5 @@ export default {
   ],
   animationOptions: { duration: 1000, fill: "both" },
 };
+
+export default backInDown;
